1143e56306908db401b24442cec4d6355d033c8f
Tarek El Lethy <tarekellethy@Maggies-MacBook-Pro.local>
2021-02-22 19:19:26 -0600

    add practice folder into repo

---
+++ practice/practice1.py
@@ -0,0 +1,20 @@
+#all this is doing is printing hello world 
+# this is a string below, you use quotations to refer to a string, a string is words 
+
+"""
+print("hello world")
+
+# lines 7-10 add two numbers together, store the result in a variable and print out the result
+a = 3
+b = 2
+c = a + b
+print(c)
+"""
+
+if 5 > 1:
+    print("Dad eats fruit")
+else: 
+    print("are you stupid, that doesnt work....")
+
+
+
